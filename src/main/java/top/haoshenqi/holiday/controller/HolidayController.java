8f68537e34f3ec484c0005ff052189530fe8d750
smileyxy <30106710+smileyxy@users.noreply.github.com>
2026-01-23 12:08:09 +0800

    Update HolidayController.java
    Enable updateNextYear

--- src/main/java/top/haoshenqi/holiday/controller/HolidayController.java
+++ src/main/java/top/haoshenqi/holiday/controller/HolidayController.java
@@ -77,10 +77,10 @@ public class HolidayController {
             return null;
         }
     }
-//    @GetMapping("update")
-//    public String updateNextYear(){
-//        holidayDateService.updateNextYear();
-//        return "success!";
-//    }
+   @GetMapping("update")
+   public String updateNextYear(){
+       holidayDateService.updateNextYear();
+       return "success!";
+   }
 
 }
